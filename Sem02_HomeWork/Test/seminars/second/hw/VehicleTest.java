7df22d1e84a291569b60b4b8cd8f234153f731fd
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2023-11-08 00:48:58 +0300

    added tests

---
+++ Sem02_HomeWork/Test/seminars/second/hw/VehicleTest.java
@@ -0,0 +1,51 @@
+package seminars.second.hw;
+
+import hw.Car;
+import hw.Motorcycle;
+import hw.Vehicle;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+    Car bmw = new Car("BMW", "X5", 2021);
+    Motorcycle honda = new Motorcycle("Honda", "CB500X", 2022);
+
+    @Test
+    public void carInstanceOfVehicle(){
+        assertInstanceOf(Vehicle.class, bmw);
+    }
+    @Test
+    public void carWheelsAmount() {
+        assertEquals(bmw.getNumWheels(), 4);
+    }
+    @Test
+    void wheelsAmountMoto(){
+        assertEquals(honda.getNumWheels(), 2);
+    }
+    @Test
+    void carSpeedTestDrive(){
+        bmw.testDrive();
+        assertEquals(bmw.getSpeed(), 60);
+    }
+    @Test
+    void motoSpeedTestDrive(){
+        honda.testDrive();
+        assertEquals(honda.getSpeed(), 75);
+    }
+    @Test
+    void carParkingSpeed(){
+        bmw.testDrive();
+        bmw.park();
+        assertEquals(bmw.getSpeed(),0);
+    }
+    @Test
+    void motoParkingSpeed(){
+        honda.testDrive();
+        honda.park();
+        assertEquals(honda.getSpeed(), 0);
+    }
+
+}
