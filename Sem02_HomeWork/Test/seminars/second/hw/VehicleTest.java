4fc6b554e518674ce712bf9f13a30a468a75a8ae
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2023-11-10 20:19:42 +0300

    added task1

--- Sem02_HomeWork/Test/seminars/second/hw/VehicleTest.java
+++ Sem02_HomeWork/Test/seminars/second/hw/VehicleTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
 
 class VehicleTest {
     Car bmw = new Car("BMW", "X5", 2021);
---
+++ Sem03/src/test/java/Task1Test.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task1Test {
+    private Task1 task1;
+    @BeforeEach
+    public void setUp(){
+        task1 = new Task1();
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 20 , 5 , 10})
+    public void getBuzz(int num){
+        assertThat(task1.fizzBuzz(num)).isEqualTo("Buzz");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {3 , 6, 9, 33})
+    public void getFizz(int num){
+        assertThat(task1.fizzBuzz(num)).isEqualTo("Fizz");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {15, 30, 60})
+    public void getfizzBuzz(int num){
+        assertThat(task1.fizzBuzz(num)).isEqualTo("FizzBuzz");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {7, 23, 47})
+    public void getNoFizzBuzz(int num){
+        assertThat(task1.fizzBuzz(num)).isEqualTo(""+num);
+    }
+}
